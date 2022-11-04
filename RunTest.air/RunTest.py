ebf7a4b37790812bbf616675f80dad2e25e28c10
kuma-100 <787811198@qq.com>
2022-11-04 19:05:53 +0800

    2022/11/04 19:05:47 优化代码

--- RunTest.air/RunTest.py
+++ RunTest.air/RunTest.py
@@ -56,14 +56,6 @@ while True:
         print("请输入正确的选项！！请重新输入！")
         continue
 
-
-# try:
-    # dev = connect_device("Android:///")
-# except:
-    # dev = connect_device("Android://127.0.0.1:5037/127.0.0.1:7555?cap_method=JAVACAP^&^&ori_method=ADBORI")
-    # dev = connect_device("Android://127.0.0.1:5037/127.0.0.1:7555")#mumu
-    # dev = connect_device("Android://127.0.0.1:5037/127.0.0.1:62001")#夜神
-
 try:
     dev.minicap.get_stream()
 except:
@@ -71,21 +63,6 @@ except:
     G.add_device(dev)
 
 auto_setup(__file__)
-
-OPEAR_CONF = {
-    "0": "启动游戏",
-    "00": "启动游戏登录账号",
-    "1": "单关卡循环刷图+自动吃药（需选中关卡）",
-    "2": "剿灭用:单关卡循环刷图（需选中关卡）",
-    "3": "自动领取任务奖励（需保证界面在主页）",
-    "4": "自动获取信用（需保证界面在主页）",
-    "5": "完成公开招募领取干员（需保证界面在主页）",
-    "6": "跳过弹窗",
-    "7": "周一批量刷剿灭脚本（老图龙门市区）",
-    "8": "周一批量刷剿灭脚本（新图轮换）",
-    "9": "愚人号:单关卡循环刷图（需选中关卡）",
-    "001": "灰蕈迷境刷蜜饼",
-}
 
 PRINT_INFO = """
 欢迎使用明日方舟护肝小能手\(^o^)/
@@ -111,39 +88,37 @@ while True:
     opear = input("")
     if "88" == opear:
         break
-    if not OPEAR_CONF.__contains__(opear):
-        print("请输入正确的选项！！请重新输入！")
-        continue
-    if "0" == opear:
+    elif "0" == opear:
         TestCase.StartGame()
-    if "00" == opear:
+    elif "00" == opear:
         TestCase.login_game()
-    if "1" == opear:
+    elif "1" == opear:
         print("请输入刷图次数：")
         cnt = input("")
         TestCase.AutoFight(int(cnt))
-    if "2" == opear:
+    elif "2" == opear:
         print("请输入刷图次数：")
         cnt = input("")
         TestCase.AutoFight(int(cnt),2)
-    if "3" == opear:
+    elif "3" == opear:
         TestCase.AutoMission()
-    if "4" == opear:
+    elif "4" == opear:
         TestCase.CreditAccess()
-    if "5" == opear:
+    elif "5" == opear:
         TestCase.CompleteOpenRecruitment()
-    if "6" == opear:
+    elif "6" == opear:
         TestCase.SkipPopup()
-    if "7" == opear:
+    elif "7" == opear:
         TestCase.MondayAutoFight(0)
-    if "8" == opear:
+    elif "8" == opear:
         TestCase.MondayAutoFight(1)
-    if "9" == opear:
+    elif "9" == opear:
         print("请输入刷图次数：")
         cnt = input("")
         TestCase.StultiferaNavis(int(cnt))
-    if "001" == opear:
+    elif "001" == opear:
         print("请输入刷图次数：")
         cnt = input("")
         TestCase.FungiMist(int(cnt))
-
+    else:
+        print("请输入正确的选项！！请重新输入！")
